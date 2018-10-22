237ed5227ff6c2375a7dd15a07af210b83dc8e81
HoldenLucas <hopalucas@gmail.com>
2018-10-22 17:12:36 -0400

    initial schema with rooms and properties tables "completed"

---
+++ inventory.sql
@@ -0,0 +1,88 @@
+-- MySQL dump 10.16  Distrib 10.1.36-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: inventory
+-- ------------------------------------------------------
+-- Server version	10.1.36-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `properties`
+--
+
+DROP TABLE IF EXISTS `properties`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `properties` (
+  `propertyCode` int(11) NOT NULL,
+  `shortName` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `longName` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`propertyCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `properties`
+--
+
+LOCK TABLES `properties` WRITE;
+/*!40000 ALTER TABLE `properties` DISABLE KEYS */;
+/*!40000 ALTER TABLE `properties` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rooms`
+--
+
+DROP TABLE IF EXISTS `rooms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rooms` (
+  `roomNumber` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `propertyCode` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `dateRecorded` date DEFAULT NULL,
+  `2Dphoto` tinyint(1) DEFAULT NULL,
+  `3Dphoto` tinyint(1) DEFAULT NULL,
+  `orientation` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `totalSeats` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `department` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `roomType` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `capacity` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `distanceLearning` tinyint(1) DEFAULT NULL,
+  `lectureCapture` tinyint(1) DEFAULT NULL,
+  `presentationCapability` tinyint(1) DEFAULT NULL,
+  `videoConferencing` tinyint(1) DEFAULT NULL,
+  `webConferencing` tinyint(1) DEFAULT NULL,
+  `wirelessAccess` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`roomNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rooms`
+--
+
+LOCK TABLES `rooms` WRITE;
+/*!40000 ALTER TABLE `rooms` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-22 17:10:44
